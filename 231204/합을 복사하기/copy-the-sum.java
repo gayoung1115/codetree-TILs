6e613d57e2b671e8fb702259c9717f649bd587b3
lee gayoung <63089026+gayoung1115@users.noreply.github.com>
2023-12-04 15:29:11 +0900

    Title: 합을 복사하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231204/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2;
+        int c =3;
+        int temp = a+b+c;
+        a=b=c=temp;
+        System.out.print(a+" "+b+" "+c);
+    }
+}
