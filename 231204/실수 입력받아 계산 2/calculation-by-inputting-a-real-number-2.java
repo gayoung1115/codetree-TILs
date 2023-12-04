1ba5b6f1436a29a56d5f84b252db9f4cd85c512d
lee gayoung <63089026+gayoung1115@users.noreply.github.com>
2023-12-04 21:14:44 +0900

    Title: 실수 입력받아 계산 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231204/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        System.out.println(a+1.5);
+
+    }
+}
