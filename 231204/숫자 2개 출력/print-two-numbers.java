1b5d37ed41af5b1fec0ef97e89ac82c6307b4235
lee gayoung <63089026+gayoung1115@users.noreply.github.com>
2023-12-04 14:25:11 +0900

    Title: 숫자 2개 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231204/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+        sb.append(3).append(" ").append(5);
+        System.out.print(sb);
+    }
+}
