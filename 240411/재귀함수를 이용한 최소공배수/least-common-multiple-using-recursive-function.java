e3fca0cb1e0df5c0cff63c796f187b6b9901bd24
lee gayoung <63089026+gayoung1115@users.noreply.github.com>
2024-04-11 17:29:45 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240411/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[n];
+        for(int i=0; i<n ;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(result(arr));
+
+    }
+
+    public static int gcd(int a, int b){
+        int r = a%b;
+        if(r==0) return b;
+        else return gcd(b, r);
+    }
+
+
+    public static int result(int []a){
+        if(a.length == 1) return a[0];
+        int m = gcd(a[0],a[1]);
+        int first = a[0]*a[1]/m ;
+        if(a.length>3){
+          for(int i=2 ;i<(a.length);i++){
+            m = gcd(first, a[i]);
+            first = first*a[i] / m ;
+        }  
+        }
+        return first;
+    }
+}
